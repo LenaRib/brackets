de201175d3af89a35ea5924edb659fb7b055e857
LenaRib <poddubnayaeo@gmail.com>
2021-07-05 12:48:47 +0200

    fix: lint

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
-module.exports = function check(str, bracketsConfig) {
-  if (str.lrngth % 2 !== 0) return false;
-  let flat = bracketsConfig.flat();
+//module.exports = 
+function check(str, bracketsConfig) {
+  console.log(str.length % 2);
+  if (str.length % 2 !== 0) return false;
+  let flat = bracketsConfig.flat().join('');
+  console.log(flat);
   while (flat) {
     let expr = flat.slice(2);
-    cinsile.log(expr);
+    if (str.lastIndexOf('(') === -1)
+      console.log(str.lastIndexOf('('));
+    console.log(str.lastIndexOf('('));
+    flat = expr;
+    console.log(expr);
   }
   return true;
 }
+
+//const config2 = [['(', ')'], ['[', ']']];
+
+//check('((()))()', config2);
